827414386afc6e3266e3a4a67dcb88c1b09305d7
Yazmin Perez <zoeperez333@gmail.com>
2022-08-11 15:22:49 -0700

    [WIP] add mainTeaList & update TeaList()

--- src/components/TeaList.js
+++ src/components/TeaList.js
@@ -1,24 +1,38 @@
 import React from "react";
 import Tea from "./Tea";
 
+const mainTeaList = [
+  {
+    name: "Zuko's breath of fire",
+    origin: "fire nation",
+    type: "chai tea",
+    price: 12.99,
+    quantity: 130,
+  },
+  {
+    name: "Appa sip sip",
+    origin: "eastern air temple",
+    type: "chamomile tea",
+    price: 12.99,
+    quantity: 130,
+  },
+];
+
 function TeaList() {
   return (
     <React.Fragment>
-    <Tea
-      name="Zuko's breath of fire"
-      origin="fire nation"
-      type="chai tea"
-      price={12.99}
-      quantity={130}
-    />
-    <Tea
-    name="Appa sip sip"
-    origin="eastern air temple"
-    type="chamomile tea"
-    price={12.99}
-    quantity={130}
-  />
-  </React.Fragment>
+      <hr />
+      {mainTeaList.map((tea, index) => (
+        <Tea
+          name={tea.name}
+          origin={tea.origin}
+          type={tea.type}
+          price={tea.price}
+          quantity={tea.quantity}
+          key={index}
+        />
+      ))}
+    </React.Fragment>
   );
 }
 
