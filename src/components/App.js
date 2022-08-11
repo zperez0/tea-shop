d35f2b3e997c0bf0ac8fa4bf5fc698720131fb6d
Yazmin Perez <zoeperez333@gmail.com>
2022-08-11 14:08:47 -0700

    [WIP] add static data & Header tag. remove app.css import

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
-import './../App.css';
+import React from 'react';
+import Header from './Header';
 
 function App() {
+  const name = "Zuko's breath of fire"
+  const origin = "fire nation"
+  const type = 'chai tea'
+  const price = 12.99
+  const quantity = 130
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+<React.Fragment>
+  <Header />
+  <h3>3a</h3>
+  <h3>{name}</h3>
+  <p>{origin}</p>
+  <p>{type}</p>
+  <p>${price}</p>
+  <p>{quantity} oz</p>
+</React.Fragment>
   );
 }
 
