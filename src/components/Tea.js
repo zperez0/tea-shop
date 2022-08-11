9bcee25f78fbce7cadf191f75263918ea3774ed5
Yazmin Perez <zoeperez333@gmail.com>
2022-08-11 15:12:06 -0700

    [WIP] add propTypes

--- src/components/Tea.js
+++ src/components/Tea.js
@@ -1,16 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Tea(props){
-
+function Tea(props) {
   return (
-<React.Fragment>
-  <h3>{props.name}</h3>
-  <p>{props.origin}</p>
-  <p>{props.type}</p>
-  <p>${props.price}</p>
-  <p>{props.quantity} oz</p>
-</React.Fragment>
+    <React.Fragment>
+      <h3>{props.name}</h3>
+      <p>{props.origin}</p>
+      <p>{props.type}</p>
+      <p>${props.price}</p>
+      <p>{props.quantity} oz</p>
+    </React.Fragment>
   );
 }
+
+Tea.propTypes = {
+  name: PropTypes.string.isRequired,
+  origin: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  quantity: PropTypes.number.isRequired
+};
 
 export default Tea;
