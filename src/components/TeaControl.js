6cc26d91d55773a4aca261d09d6d5f5085547bb6
Yazmin Perez <zoeperez333@gmail.com>
2022-08-11 16:13:51 -0700

    [WIP] add basic outline for NewTeaForm & TeaControl.

---
+++ src/components/TeaControl.js
@@ -0,0 +1,23 @@
+import React from "react";
+import NewTeaForm from "./NewTeaForm";
+import TeaList from "./TeaList";
+
+class TeaControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+export default TeaControl;
