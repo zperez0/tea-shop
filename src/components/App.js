689f75a0fb3a0afbf1992da50c834af00f92ae3f
Yazmin Perez <zoeperez333@gmail.com>
2022-08-11 16:10:47 -0700

    remove TeaList & add TeaControl tag/import

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from './Header';
-import TeaList from './TeaList';
+import TeaControl from './TeaControl';
 
 function App() {
 
   return (
 <React.Fragment>
   <Header />
-  <TeaList />
+  <TeaControl />
 </React.Fragment>
   );
 }
