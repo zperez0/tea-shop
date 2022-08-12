dcbf23916319c24be53dc7053d2d8715bae8e250
Yazmin Perez <zoeperez333@gmail.com>
2022-08-11 18:37:56 -0700

    add tea prop & details

--- src/components/TeaDetail.js
+++ src/components/TeaDetail.js
@@ -1,10 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function TeaDetail(props) {
-  return(
+  const { tea } = props;
+
+  return (
     <React.Fragment>
       <h1>🍵Tea Detail🍵</h1>
+      <h3>{tea.name}</h3>
+      <p>{tea.origin}</p>
+      <p>{tea.type}</p>
+      <p>${tea.price}</p>
+      <p>{tea.quantity} oz</p>
       <hr />
     </React.Fragment>
   );
 }
+
+TeaDetail.propTypes = {
+  tea: PropTypes.object,
+};
+
+export default TeaDetail;
