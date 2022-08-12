52858ad0f6c26c3842de0b512b9829635fa11e24
Yazmin Perez <zoeperez333@gmail.com>
2022-08-11 21:44:32 -0700

    add availability

--- src/components/TeaDetail.js
+++ src/components/TeaDetail.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function TeaDetail(props) {
-  const { tea, onClickingDelete, onPurchasingTea} = props;
+  const { tea, onClickingDelete, onPurchasingTea } = props;
 
   return (
     <React.Fragment>
@@ -10,15 +10,18 @@ function TeaDetail(props) {
       <h3>{tea.name}</h3>
       <p>{tea.origin}</p>
       <p>{tea.type}</p>
-      <p>${tea.price}</p>
-      <p>{tea.quantity} lbs</p>
+      <p>${tea.price} per oz</p>
+      <p>
+        Availability: {""}
+        {tea.quantity > 0 ? tea.quantity + " oz" : "Out of Stock"}
+      </p>
       <button onClick={props.onClickingEdit}>Update Tea</button>
       <br />
       <br />
       <button onClick={() => onClickingDelete(tea.id)}>❌Delete Tea❌</button>
       <br />
       <br />
-      <button onClick={ () => onPurchasingTea(tea.id)}>Purchase</button>
+      <button onClick={() => onPurchasingTea(tea.id)}>Purchase</button>
       <hr />
     </React.Fragment>
   );
@@ -28,7 +31,7 @@ TeaDetail.propTypes = {
   tea: PropTypes.object,
   onClickingDelete: PropTypes.func,
   onClickingEdit: PropTypes.func,
-  onClickingPurchase: PropTypes.func
+  onClickingPurchase: PropTypes.func,
 };
 
 export default TeaDetail;
