8edb4aac55b03c1ef7466b61253457c00b10d578
Yazmin Perez <zoeperez333@gmail.com>
2022-08-11 19:33:54 -0700

    add EditTeaForm file & include ReusableForm

---
+++ src/components/EditTeaForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+
+function EditTeaForm(props) {
+  return (
+    <React.Fragment>
+      <ReusableForm buttonText="Update Tea" />
+    </React.Fragment>
+  );
+}
+
+export default EditTeaForm;
