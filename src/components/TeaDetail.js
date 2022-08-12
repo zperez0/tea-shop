8d7a5e4fff920a30c2aec45bb00b5e28dd9f8200
Yazmin Perez <zoeperez333@gmail.com>
2022-08-12 00:46:43 -0700

    add Card & styling to buttons, font, & form

--- src/components/TeaDetail.js
+++ src/components/TeaDetail.js
@@ -1,28 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Card from "./UI/Card";
 
 function TeaDetail(props) {
   const { tea, onClickingDelete, onPurchasingTea } = props;
 
   return (
     <React.Fragment>
-      <h1>🍵Tea Detail🍵</h1>
-      <h3>{tea.name}</h3>
-      <p>{tea.origin}</p>
-      <p>{tea.type}</p>
-      <p>${tea.price} per oz</p>
-      <p>
-        Availability: {""}
-        {tea.quantity > 0 ? tea.quantity + " oz" : "Out of Stock"}
-      </p>
-      <button onClick={props.onClickingEdit}>Update Tea</button>
-      <br />
-      <br />
-      <button onClick={() => onClickingDelete(tea.id)}>❌Delete Tea❌</button>
-      <br />
-      <br />
-      <button onClick={() => onPurchasingTea(tea.id)}>Purchase</button>
-      <hr />
+      <div className="detail">
+        <Card>
+          <h1>Tea Detail</h1>
+          <h2>{tea.name}</h2>
+          <p>{tea.origin}</p>
+          <p>{tea.type}</p>
+          <p>${tea.price} per oz</p>
+          <p>
+            Availability: {""}
+            {tea.quantity > 0 ? tea.quantity + " oz" : "Out of Stock"}
+          </p>
+          <div className="btn-detail">
+            <button onClick={props.onClickingEdit}>Update Tea</button>
+            <button onClick={() => onClickingDelete(tea.id)}>Delete Tea</button>
+            <button onClick={() => onPurchasingTea(tea.id)}>Purchase</button>
+          </div>
+        </Card>
+      </div>
     </React.Fragment>
   );
 }
