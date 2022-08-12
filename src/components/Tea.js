3b76d74a4f5666b629e07396499c8d815520d649
Yazmin Perez <zoeperez333@gmail.com>
2022-08-11 18:43:29 -0700

    fix props typo to whenTeaClicked

--- src/components/Tea.js
+++ src/components/Tea.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 function Tea(props) {
   return (
     <React.Fragment>
-      <div onClick={() => props.whenTicketClicked(props.id)}>
+      <div onClick={() => props.whenTeaClicked(props.id)}>
         <h3>{props.name}</h3>
       </div>
     </React.Fragment>
