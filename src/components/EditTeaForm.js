b04a2b3a9af5d89e6314efb860849b3dd693ba2b
Yazmin Perez <zoeperez333@gmail.com>
2022-08-11 19:55:52 -0700

    add prop onEditTea & tea

--- src/components/EditTeaForm.js
+++ src/components/EditTeaForm.js
@@ -1,12 +1,34 @@
 import React from "react";
 import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
 
 function EditTeaForm(props) {
+  const { tea } = props;
+
+  function handledEditTeaFormSubmission(event) {
+    event.preventDefault();
+    props.onEditTea({
+      name: event.target.name.value,
+      origin: event.target.origin.value,
+      type: event.target.type.value,
+      price: parseInt(event.target.price.value),
+      quantity: parseInt(event.target.quantity.value),
+      id: tea.id,
+    });
+  }
   return (
     <React.Fragment>
-      <ReusableForm buttonText="Update Tea" />
+      <ReusableForm
+        formSubmissionHandler={handledEditTeaFormSubmission}
+        buttonText="Update Tea"
+      />
     </React.Fragment>
   );
 }
+
+EditTeaForm.propTypes = {
+  tea: PropTypes.object,
+  onEditTea: PropTypes.func,
+};
 
 export default EditTeaForm;
